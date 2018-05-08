d0c4608e77bea166fb983713a433f858f963b9f4
Carlos Rojas <carlos.rojas.burgos@gmail.com>
2018-05-08 00:25:46 -0300

    Added method my_books and the view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -62,6 +62,10 @@ class BooksController < ApplicationController
     end
   end
 
+  def my_books
+    @books = current_user.books
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
